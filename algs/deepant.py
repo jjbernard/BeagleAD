00dbbfeca0dabb34f924dda9b0577e6efbb04bce
Jean-Jacques Bernard <jjb@cantab.net>
2019-04-22 21:39:08 +0200

    Update deepant.py, need to finalize the size of the FC layer

--- algs/deepant.py
+++ algs/deepant.py
@@ -18,31 +18,37 @@ class Predictor(nn.Module):
         Each convolutional layer is composed of 32 filters. 
         We use a stride of 1 and no padding for the convolutional layers
         Args:
-            - ts_dim
+            - nb_ts
             - nb_filters
             - kernel_size_conv
             - kernel_size_pool
+            - predict_window
     """
-    def __init__(self, ts_dim, nb_filters, kernel_size_conv, kernel_size_pool):
+    def __init__(self, nb_ts, nb_filters, kernel_size_conv, kernel_size_pool, predict_window):
         super(Predictor, self).__init__()
         # Input for nn.Conv1d() is nb input channels, nb output channels, kernel size
-        # Here, the number of input channels is actually the number of time series
-        # So for univariate time series, nb input channels is 1, and so on.
-        self.ts_dim = ts_dim
+        # The number of channel is going to be 1
+        self.nb_ts = nb_ts
         self.nb_filters = nb_filters
         self.kernel_size_conv = kernel_size_conv
         self.kernel_size_pool = kernel_size_pool
-        self.conv1 = nn.Conv1d(ts_dim, nb_filters, kernel_size_conv, padding=1, bias=True)
+        self.predict_window = predict_window
+        self.conv1 = nn.Conv1d(1, nb_filters, kernel_size_conv, padding=1, bias=True)
         self.maxpool1 = nn.MaxPool1d(kernel_size_pool)
-        # Need the equivalent of ts_dim here... Experiment a bit!
-        self.conv2 = nn.Conv1d(ts_dim,nb_filters, kernel_size_conv, bias=True)
+        self.conv2 = nn.Conv1d(nb_filters,nb_filters, kernel_size_conv, padding=1, bias=True)
         self.maxpool2 = nn.MaxPool1d(kernel_size_pool)
-        self.fc = nn.Linear()
+        # Need a way to calculate the size of the series after all the previous operations
+        self.fc = nn.Linear(,predict_window)
 
     def forward(self, input):
         out = self.maxpool1(F.relu(self.conv1(input)))
         out2 = self.maxpool2(F.relu(self.conv2(out)))
-       
-        result = nn.Linear(out2)
+    
+        # Need to call out2.view(nb_ts,-1) to flatten before calling nn.Linear()
+        # with nb_ts which is the number of time series (i.e. 1 for univariate 
+        # and more than one for multivariate)
+        out2 = out2.view(self.nb_ts, -1)
+        
+        result = F.relu(nn.Linear(out2))
 
         return result
